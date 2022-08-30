a30cce5b2e36034303c6542d0de95ae4507c588d
sandrojr <sjr1999@protonmail.com>
2022-08-30 17:26:02 -0300

    find einstein

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use LdapRecord\Models\OpenLDAP\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $user = User::find('uid=einstein,dc=example,dc=com');
+
+    dd($user);
+
     return view('welcome');
 });
